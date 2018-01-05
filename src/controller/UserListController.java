e6ffc437f4f54d0f81bf5fa23422d09c9c5cf4b4
Igor Lipko <igorlipko75@gmail.com>
2018-01-05 20:01:34 +0300

    Add UserListView and UserListController where HTML out

--- src/controller/UserListController.java
+++ src/controller/UserListController.java
@@ -37,41 +37,8 @@ public class UserListController extends HttpServlet {
             UserServiceImpl service = new UserServiceImpl();
             service.setUserDao(dao);
             List<User> users = service.findAll();
-            resp.setCharacterEncoding("UTF-8");
-            PrintWriter writer = resp.getWriter();
-            writer.println("<!DOCTYPE html>");
-            writer.println("<html>");
-            writer.println("<head>");
-            writer.println("<meta charset=\"UTF-8\">");
-            writer.println("<title>Список пользователей</title>");
-            writer.println("<link href=\"../main.css\" rel=\"stylesheet\">");
-            writer.println("</head>");
-            writer.println("<body>");
-            writer.println("<h1>Система «Автобаза»</h1>");
-            writer.println("<h2>Список пользователей</h2>");
-            writer.println("<table>");
-            writer.println("<tr>");
-            writer.println("<th>Имя пользователя</th>");
-            writer.println("<th>Фамилия</th>");
-            writer.println("<th>Имя</th>");
-            writer.println("<th>Отчество</th>");
-            writer.println("<th>Роль</th>");
-            writer.println("<td>&nbsp;</td>");
-            writer.println("</tr>");
-            for(User user: users) {
-                writer.println("<tr>");
-                writer.printf("<td class=\"content\">%s</td>\n", user.getLogin());
-                writer.printf("<td class=\"content\">%s</td>\n", user.getLastName());
-                writer.printf("<td class=\"content\">%s</td>\n", user.getFirstName());
-                writer.printf("<td class=\"content\">%s</td>\n", user.getMiddleName());
-                writer.printf("<td class=\"content\">%s</td>\n", user.getRole().getName());
-                writer.printf("<td class=\"empty\"><a href=\"edit.html?id=%d\" class=\"edit\"></a></td>\n", user.getId());
-                writer.println("</tr>");
-            }
-            writer.println("</table>");
-            writer.println("<a href=\"edit.html\" class=\"add-button\">Добавить</a>");
-            writer.println("</body>");
-            writer.println("</html>");
+            req.setAttribute("users", users);
+            req.getRequestDispatcher("/WEB-INF/user/list.html").forward(req, resp);
         } catch(SQLException | ServiceException e) {
             throw new ServletException(e);
         } finally {
